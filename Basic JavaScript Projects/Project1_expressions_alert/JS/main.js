e474e503da6e0cd89eaa9c1616d23ff45e58281a
crainfam4 <crainfam4@gmail.com>
2021-09-16 22:22:16 -0700

    Hopefully fixed concatenating issue

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -11,12 +11,18 @@ document.write("\"I looked everywhere for Sally\'s purple shirt,\" sighed Mom. "
 
 document.write("I am retraining my cats to use the toilet. " + "So far there have been two potties in the toilet today. ");  //concatenated string
 
-var C = "I do not know what to do with our dog. " + "She has inflammatory bowel disease, and when she has kibble of any kind, she has liquid diarrhea. " + "We have been feeding \"The Farmer\'s Dog\", which nearly solves the problem, but she steals food because she is so hungry, and has lost at least five pounds in about a month. ";  //setting a variable with a concatenated string
+var C = "I do not know what to do with our dog. " + "She has inflammatory bowel disease, and when she has kibble of any kind, she has liquid diarrhea. ";  //setting a variable with a concatenated string
+var D = "We have been feeding \"The Farmer\'s Dog\", which nearly solves the problem, but she steals food because she is so hungry, and has lost at least five pounds in about a month. ";  //setting a variable with a string
 var C = C.fontcolor("Blue");             //setting the font color for the variable C
-document.write(C);                       //displaying concatenated variable C with blue font
+var D = D.fontcolor("Orange");           //setting the font color for the variable D
+document.write(C + D);                   //displaying concatenated variable C + D with blue and orange font
 
 var Family = "The Crains", Dad = "Kevin", Mom = "Angela", Cat = "Hiro", Daughter = "Acacia", Dog = "Luna", Cat = "Mystic", FutureSon = "KJ";  //setting multiple variables a once
 var Daughter = Daughter.fontcolor("pink");  //setting font color for variable Daughter
 document.write(Daughter);                //displaying the pink return of the variable Daughter
 
 document.write(9*3);                     //displaying an expression
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
