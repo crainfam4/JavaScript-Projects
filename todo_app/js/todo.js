03c2c1851e66337afe6969f865481ead69d54114
crainfam4 <crainfam4@gmail.com>
2021-12-31 20:54:54 -0800

    todo app

--- todo_app/js/todo.js
+++ todo_app/js/todo.js
@@ -1,60 +1,51 @@
-/* Create a "close" button and append it to each list item */
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+/* This function gets the task from input */
+function get_todos() {
+    /* This creates an array of tasks that are inputed */
+    var todos = new Array;
+    /* This pulls the task that was saved in the web browser memory */
+    var todos_str = localStorage.getItem('todo');
+    /* If the input is not null then JSON.parse will
+    communicate with the web browser to make the task a JavaScript object. */
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
 }
+/* This function adds the inputed task to the get_todos function array */
+function add() {
+    /* This takes the inputed task and creates a variable of it */
+    var task = document.getElementById('task').ariaValueMax;
 
-/* Click on a close button to hide the current list item */
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-/* Add a "checked" symbol when clicking on a list item */
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
-/* This displays the inputed task when the 'Add Item' button is clicked */
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("task").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("Please type a To Do Item.");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
+    var todos = get_todos();
+    /* This adds a new task to the end of the array */
+    todos.push(task);
+    /* This converts the task input to a JSON string */
+    localStorage.setItem('todo', JSON.stringify(todos));
     document.getElementById("task").value = "";
+    show();
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
+    return false;
 }
 
-/* Set Item to local storage */
-localStorage.setItem("myUL", JSON.stringify("myUL"));
-/* Retrieve item from local storage */
-var myUL = JSON.parse(localStorage.getItem("myUL"));
+/* This function keeps the tasks permanently displayed on the screen */
+function show() {
+    /* This sets the task that was retrieved as a variable */
+    var todos = get_todos();
+
+    /*This sets up each task as an unordered list */
+    var html = '<ul>';
+    /* This displays a task to the list in the order that it is inputed */
+    for (var i = 0; i < todos.length; i++) {
+        /* This also displays the task as a list and creates the button with the "x" */
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
+
+    };
+    html += '</ul>';
+    /* This displays the task as a list */
+    document.getElementById('todos').innerHTML = html;
+
+}
+/* This displays the inputed task when the 'Add Item' button is clicked */
+document.getElementById('add').addEventListener('click'), add);
+/* This will keep the inputs displayed permanently on the screen */
+show();
